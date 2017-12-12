957de28dbc4499d98396943bb3c087654c14ef6e
emfritz2 <emfritz2@gmail.com>
2017-12-11 18:14:57 -0600

    friend finder app

---
+++ routing/apiRoutes.js
@@ -0,0 +1,11 @@
+
+  var friendData = require("../app/data/friends");
+
+
+  module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  	}); 
+
+	};
