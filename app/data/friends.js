de5836e29e82947c3eafc8949955a4134d152e0d
emfritz2 <emfritz2@gmail.com>
2017-12-12 20:47:08 -0600

    added friend array

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+
+var friendData = [
+  {
+    name: "The Grinch",
+    photo:"http://schmoesknow.com/wp-content/uploads/2014/12/how-the-grinch-stole-christmas-7.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+
+
+module.exports = friendData;
+
+// console.log(friendData);
